dc3511c83bbde1f9f4e09b3f8630f066560d8291
Patrick Caminhas <patrickcaminhasm@gmail.com>
2023-01-22 03:10:31 -0300

    creation of login pages and registration page
    creation of login and registration pages for the customer user and login page for the employee user..

--- Projeto/Codigo/js/script.js
+++ Projeto/Codigo/js/script.js
@@ -1,16 +1,38 @@
-function clickMenu(){
-    if(itens.style.display == 'block'){
+function clickMenu() {
+    if (itens.style.display == 'block') {
         itens.style.display = 'none'
-    }else{
+    } else {
         itens.style.display = 'block'
     }
 }
-
- function closeTeste() {
-    teste.style.display='none'
-    testeback.style.display='none'
+function closeTeste() {
+    teste.style.display = 'none'
+    testeback.style.display = 'none'
+    titulocaixalogin.style.display='block'
 }
-function openTeste(){
-    teste.style.display='block'
-    testeback.style.display='block'
+function openTeste() {
+    teste.style.display = 'block'
+    testeback.style.display = 'block'
+    titulocaixalogin.style.display= 'none'
+}
+function changeToLogin() {
+    titulobox.style.height = '30vh'
+    tituloimg.style.height = '30vh'
+    indexbox.style.height = '65vh'
+    indexbox.style.flexWrap = 'nowrap'
+    leftSide.style.borderWidth = '0px'
+    botaoLogin.style.fontSize = '2vh'
+    botaoReset.style.marginRight = '0vw'
+    botaoReset.style.fontSize = '2vh'
+}
+function changetoRegister() {
+    titulobox.style.height = '30vh'
+    tituloimg.style.height = '30vh'
+    indexbox.style.height = '65vh'
+    indexbox.style.flexWrap = 'nowrap'
+    leftSide.style.borderWidth = '0px'
+    botaoLogin.style.fontSize = '2vh'
+    botaoReset.style.marginRight = '0vw'
+    botaoReset.style.fontSize = '2vh'
+    nome.style.fontSize = '5vh'
 }
