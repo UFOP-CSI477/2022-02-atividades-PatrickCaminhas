1017536c6c4b713e31b1a8ab1dbdffe7a0513217
Patrick Caminhas <patrickcaminhasm@gmail.com>
2023-01-23 12:14:13 -0300

    changing the location of login and registration
    login and registration system on the home page instead of separate pages.

--- Projeto/Codigo/js/script.js
+++ Projeto/Codigo/js/script.js
@@ -10,11 +10,42 @@ function closeTeste() {
     testeback.style.display = 'none'
     titulocaixalogin.style.display='block'
 }
+
 function openTeste() {
     teste.style.display = 'block'
     testeback.style.display = 'block'
     titulocaixalogin.style.display= 'none'
 }
+
+
+function closeLogin() {
+    loginBox.style.display = 'none'
+    loginBack.style.display = 'none'
+    titulocaixalogin.style.display='block'
+}
+
+function openLogin() {
+    loginBox.style.display = 'block'
+    loginBack.style.display = 'block'
+    titulocaixalogin.style.display= 'none'
+}
+
+
+
+function closeRegister() {
+    registerBox.style.display = 'none'
+    registerBack.style.display = 'none'
+    titulocaixalogin.style.display='block'
+}
+
+function openRegister() {
+    registerBox.style.display = 'block'
+    registerBack.style.display = 'block'
+    titulocaixalogin.style.display= 'none'
+}
+
+
+
 function changeToLogin() {
     titulobox.style.height = '30vh'
     tituloimg.style.height = '30vh'
@@ -22,8 +53,8 @@ function changeToLogin() {
     indexbox.style.flexWrap = 'nowrap'
     leftSide.style.borderWidth = '0px'
     botaoLogin.style.fontSize = '2vh'
-    botaoReset.style.marginRight = '0vw'
-    botaoReset.style.fontSize = '2vh'
+    botaoForget.style.marginRight = '0vw'
+    botaoForget.style.fontSize = '2vh'
 }
 function changetoRegister() {
     titulobox.style.height = '30vh'
@@ -31,7 +62,7 @@ function changetoRegister() {
     indexbox.style.height = '65vh'
     indexbox.style.flexWrap = 'nowrap'
     leftSide.style.borderWidth = '0px'
-    botaoLogin.style.fontSize = '2vh'
+    botaoRegister.style.fontSize = '2vh'
     botaoReset.style.marginRight = '0vw'
     botaoReset.style.fontSize = '2vh'
     nome.style.fontSize = '5vh'
